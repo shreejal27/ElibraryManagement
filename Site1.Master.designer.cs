de43d39e061291e0a85bab428ea672cb4e4d52ce
Shreejal Maharjan <shreejal27@gmail.com>
2024-02-04 22:38:55 +0545

    revisiting the project

--- Site1.Master.designer.cs
+++ Site1.Master.designer.cs
@@ -33,6 +33,15 @@ namespace ElibraryMangement
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// LinkButton4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton4;
+
+        /// <summary>
         /// LinkButton1 control.
         /// </summary>
         /// <remarks>
