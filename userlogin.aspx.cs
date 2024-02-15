58885a5a8d875a053cf6107011ff87f14975a74c
Shreejal Maharjan <shreejal27@gmail.com>
2024-02-15 23:59:33 +0545

    member login successful

--- userlogin.aspx.cs
+++ userlogin.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +10,11 @@ using System.Web.UI.WebControls;
 
 namespace ElibraryMangement
 {
+
     public partial class userlogin : System.Web.UI.Page
     {
+        
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -17,8 +23,40 @@ namespace ElibraryMangement
         //user login
         protected void LoginBtnClick(object sender, EventArgs e)
         {
-            Response.Write("<script> alert('Button Clicked');</script>");
+            //Response.Write("<script> alert('Button Clicked');</script>");
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == System.Data.ConnectionState.Closed)
+                {
+                    con.Open();
+                }
 
+                SqlCommand cmd = new SqlCommand(" SELECT * FROM member_master_tbl WHERE member_id = '"+ TextBox1.Text.Trim() + "' AND password = '" + TextBox2.Text.Trim() + "';", con);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows){ 
+                    while (dr.Read())
+                    {
+                       
+                     Response.Write("<script> alert(' " + dr.GetValue(8).ToString() + "');</script>");
+
+                    }
+                }
+                else
+                {
+                  Response.Write("<script> alert('Invalid credentials');</script>");
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write("<script> alert(' " + ex.Message + "');</script>");
+
+            }
         }
+
     }
 }
